c54909b0ba1841559d12aa1637a95070a2ee0936
Thanasis <hello@thanpa.com>
2019-01-31 12:45:51 +0200

    Added phpdocs in exceptions

--- src/Exceptions/UnsupportedPaymentMethodException.php
+++ src/Exceptions/UnsupportedPaymentMethodException.php
@@ -1,6 +1,10 @@
 <?php
 namespace Buckaroo\Exceptions;
 
+/**
+ * This exception is thrown when the payment method that the developer
+ * provided is not recognizable by this SDK.
+ */
 class UnsupportedPaymentMethodException extends \Exception
 {
 
